10743b53d52ea808f734d9fa54c8369a634667ac
Byfurkanben <135124958+Byfurkanben@users.noreply.github.com>
2024-03-25 00:13:34 +0300

    Update enough.py

--- enough.py
+++ enough.py
@@ -24,7 +24,7 @@ while 1:
                                __/ |      
                               |___/      
     
-    Sms: {}           {}coder {}byfurkan\n  
+    Sms: 49{}           {}coder {}byfurkan\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo😈)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
